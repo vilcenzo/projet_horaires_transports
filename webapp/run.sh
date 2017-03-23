ecedd951495890c0b23a402baf985b4e4a491352
Vincent Dejouy <vdejouy@quantmetry.com>
2017-03-23 16:40:36 +0100

    nouvelle organisation de fichiers

--- webapp/run.sh
+++ webapp/run.sh
@@ -1,4 +1,4 @@
-export FLASK_APP=~/projet_horaires_transports/webapp/project_horaires_trains/horairesflaskapp
+export FLASK_APP=~/projet_horaires_transports/webapp/autoapp.py
 export FLASK_DEBUG=1
 flask run
 
